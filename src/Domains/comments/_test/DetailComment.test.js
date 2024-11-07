accd6ec975f1823cac8f63689acd0603b32257fc
buhorii26 <buhorics@gmail.com>
2024-11-07 13:59:44 +0900

    membuat testing code pada DetailComment.test.js

--- src/Domains/comments/_test/DetailComment.test.js
+++ src/Domains/comments/_test/DetailComment.test.js
@@ -69,4 +69,45 @@ describe('DetailComment entities', () => {
     expect(content).toEqual('**komentar telah dihapus**');
     expect(replies).toEqual(payload.replies);
   });
+
+  // Pengujian untuk metode _verifyPayload
+  it('should set replies to empty array when replies property is not provided', () => {
+    const payload = {
+      id: 'comment-123',
+      username: 'user-456',
+      date: '2022-09-09T09:15:30.338Z',
+      content: 'comment yang bagus!',
+    };
+
+    const { replies } = new DetailComment(payload);
+
+    expect(replies).toStrictEqual([]);
+  });
+
+  // Pengujian untuk metode _remapPayload
+  it('should remap content correctly based on isDelete value', () => {
+    const payloadWithDeleteTrue = {
+      id: 'comment-123',
+      username: 'user-456',
+      date: '2022-09-09T09:15:30.338Z',
+      content: 'Komen ini telah dihapus',
+      replies: [],
+      isDelete: true,
+    };
+
+    const payloadWithDeleteFalse = {
+      id: 'comment-123',
+      username: 'user-456',
+      date: '2022-09-09T09:15:30.338Z',
+      content: 'komen ini tersembunyi',
+      replies: [],
+      isDelete: false,
+    };
+
+    const deletedComment = new DetailComment(payloadWithDeleteTrue);
+    const visibleComment = new DetailComment(payloadWithDeleteFalse);
+
+    expect(deletedComment.content).toEqual('**komentar telah dihapus**');
+    expect(visibleComment.content).toEqual(payloadWithDeleteFalse.content);
+  });
 });
